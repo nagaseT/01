47eb37c60f8f899a0fface6e6a7f28dc5224d9bf
Tatsuya NAGASE <singyesterdayforme0420@gmail.com>
2015-06-12 00:42:21 +0900

    add validate

--- src/form.js
+++ src/form.js
@@ -1,45 +1,58 @@
-var USERNAME_MAX = 8;
-var USERNAME_MIN = 2;
+var USERNAME_CHECK = {
+			maxStr: 8,
+			minStr: 2,
+			unallowedCharacters: new RegExp("[^a-z-]", "g"),
+			numberErrorMessage: 'ERROR : usernameは2文字以上8文字以下です。',
+			typeErrorMessage: 'ERROR : usernameに使用できるのは英小文字と - のみです。'
+		};
+var PASSWORD_CHECK = {
+			maxStr: 24,
+			minStr: 6,
+			unallowedCharacters: new RegExp("[^a-zA-Z-+!@]", "g"),
+			numberErrorMessage: 'ERROR : passwordは6文字以上24文字以下です。',
+			typeErrorMessage: 'ERROR : passwordに使用できるのは英大小字, -, +, !, @ のみです。'
+		};
 
-function validate(){
+function checkInput(){
   this.clearAlert();
-  var flag1 = this.checkUsername();
-  if(!flag1){
-    this.addAlert();
+  var checkUsernameParams = USERNAME_CHECK;
+  checkUsernameParams.inputStr = document.form_login.field_username.value;
+  var checkUsernameResult = this.validate(checkUsernameParams);
+  if(!checkUsernameResult.result){
+    this.addAlert(checkUsernameResult.errorMessage);
+    return false;
+  }
+  var checkPasswordParams = PASSWORD_CHECK;
+  checkPasswordParams.inputStr = document.form_login.field_password.value;
+  var checkPasswordResult = this.validate(checkPasswordParams);
+  if(!checkPasswordResult.result){
+    this.addAlert(checkPasswordResult.errorMessage);
     return false;
   }
   return true;
-  /*
-  var username = document.form_login.field_username.value;
-  var password = document.form_login.field_password.value;
-  if(username === ''){
-    //window.alert('no userneme');
-    this.addAlert();
-    return false;
-  }
-  if(password === ''){
-    //window.alert('no password');
-    this.addAlert();
-    return false;
-  }
-  return true;
-  */
 }
 
-function checkUsername(){
-  var username = document.form_login.field_username.value;
-  var length = username.length;
-  if (length < USERNAME_MIN || USERNAME_MAX < length){
-    return false;
+function validate(params){
+  var length = params.inputStr.length;
+  
+  if (length < params.minStr || params.maxStr < length){
+	var results = {result: false, errorMessage: params.numberErrorMessage};
+    return results;
   } else {
-    return true;
+  	var hoge = params.inputStr.match(params.unallowedCharacters);
+  	if (hoge){
+  		var results = {result: false, errorMessage: params.typeErrorMessage};
+  		return results;
+  	}
+  	var results = {result: true, errorMessage: ''};
+    return results;
   }
 }
 
-function addAlert(){
+function addAlert(errorMessage){
   var addObj = document.createElement("div");
   addObj.setAttribute("id", "alert_txt");
-  addObj.innerHTML = '<p>add</p>';
+  addObj.innerHTML = '<p>'+errorMessage+'</p>';
   var parentObj = document.getElementById("alert_area");
   parentObj.appendChild(addObj);
 }
