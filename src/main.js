080072f22978e5885fc99fb582b375da814ab7ee
Tatsuya Nagase <tatsuya.nagase@nts.ricoh.co.jp>
2015-06-22 18:07:45 +0900

    separated

--- src/main.js
+++ src/main.js
@@ -1,24 +1,20 @@
 window.addEventListener('load', function(e) {
   var $login = document.getElementById('login');
+  var $errors = document.getElementById('errors');
 
   $login.addEventListener('submit', function(e){
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
-
-    console.log(username, password);
-
     var params = {username: username, password: password};
-    console.log(params);
+
+    while($errors.firstChild) {
+      $errors.removeChild($errors.firstChild);
+    }
+
     var results = validation(params);
-    console.log(results);
 
-    if (results.length !== 0) {
+    if (results.length > 0) {
       e.preventDefault();
-      var $errors = document.getElementById('errors');
-
-      while($errors.firstChild) {
-        $errors.removeChild($errors.firstChild);
-      }
 
       var length = results.length;
       for (var i = 0; i < length; i++) {
--- src/validation.js
+++ src/validation.js
@@ -2,6 +2,7 @@ var USERNAME_CHECK = {
   maxStr: 8,
   minStr: 2,
   unallowedCharacters: new RegExp('[^a-z-]', 'g'),
+  noInputErrorMessage: 'ERROR : usernameを入力して下さい。',
   numberErrorMessage: 'ERROR : usernameは2文字以上8文字以下です。',
   typeErrorMessage: 'ERROR : usernameに使用できるのは英小文字と - のみです。'
 };
@@ -9,6 +10,7 @@ var PASSWORD_CHECK = {
   maxStr: 24,
   minStr: 6,
   unallowedCharacters: new RegExp('[^a-zA-Z-\+!@]', 'g'),
+  noInputErrorMessage: 'ERROR : passwordを入力して下さい。',
   numberErrorMessage: 'ERROR : passwordは6文字以上24文字以下です。',
   typeErrorMessage: 'ERROR : passwordに使用できるのは英大小字, -, +, !, @ のみです。'
 };
@@ -32,22 +34,30 @@ function validation(params) {
 
   var username = params.username;
   var usernameLength = username.length;
-  if (usernameLength < USERNAME_CHECK.minStr || USERNAME_CHECK.maxStr < usernameLength) {
-    messages.push(USERNAME_CHECK.numberErrorMessage);
-  }
-  var usernameMatching = username.match(USERNAME_CHECK.unallowedCharacters);
-  if (usernameMatching){
-    messages.push(USERNAME_CHECK.typeErrorMessage);
+  if (usernameLength === 0) {
+    messages.push(USERNAME_CHECK.noInputErrorMessage);
+  } else {
+    if (usernameLength < USERNAME_CHECK.minStr || USERNAME_CHECK.maxStr < usernameLength) {
+      messages.push(USERNAME_CHECK.numberErrorMessage);
+    }
+    var usernameMatching = username.match(USERNAME_CHECK.unallowedCharacters);
+    if (usernameMatching){
+      messages.push(USERNAME_CHECK.typeErrorMessage);
+    }    
   }
 
   var password = params.password;
   var passwordLength = password.length;
-  if (passwordLength < PASSWORD_CHECK.minStr || PASSWORD_CHECK.maxStr < passwordLength) {
-    messages.push(PASSWORD_CHECK.numberErrorMessage);
-  }
-  var passwordMatching = password.match(PASSWORD_CHECK.unallowedCharacters);
-  if (passwordMatching){
-    messages.push(PASSWORD_CHECK.typeErrorMessage);
+  if (passwordLength === 0) {
+    messages.push(PASSWORD_CHECK.noInputErrorMessage);
+  } else {
+    if (passwordLength < PASSWORD_CHECK.minStr || PASSWORD_CHECK.maxStr < passwordLength) {
+      messages.push(PASSWORD_CHECK.numberErrorMessage);
+    }
+    var passwordMatching = password.match(PASSWORD_CHECK.unallowedCharacters);
+    if (passwordMatching){
+      messages.push(PASSWORD_CHECK.typeErrorMessage);
+    }
   }
 
   return messages;
